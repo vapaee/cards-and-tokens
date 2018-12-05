8f97a69d424356e5d63eb60c2be2d9d30fb32f60
Viterbo <viter.rod@gmail.com>
2018-12-05 02:46:24 -0300

    detalle

---
+++ config.init.php
@@ -0,0 +1,33 @@
+<?php
+$config = array();
+$config["site_url"] = "http://cardsandtokens.com";
+$config["site_name"] = "Cards & Tokens";
+$config["site_social_image"] = "http://app.cardsandtokens.com/assets/cards-and-tokens.jpg";
+$config["image_size"] = array(
+    "width" => "670",
+    "height" => "380"
+);
+$config["description"] = "A place where you can create, trade and collect cards of any topic and make money in the process.";
+$config["favicon_url"] = "assets/chip-16.png";
+$config["favicon_type"] = "image/png";
+$config["locale"] = "en_us";
+$config["google_app_id"] = "UA-71862452-1";
+$config["fb_app_id"] = "1658698094394840";
+$config["fb_page"] = "cardsandtokens";
+$config["vapaee_client_id"] = "f47ebe1ab2f388e549df8570833e18fa";
+$config["google_client_id"] = '1079652811584-1a0710pi1pcvlnlait89dbadlo6p5emm.apps.googleusercontent.com';
+$config["app_url_default"] = "/home";
+$config["app_module_name"] = "app.cnt";
+$config["upload_folder"] = "/var/www/data/cardsandtokens";
+$config["user_service"] = "VapaeeUser";
+$config["userdata_service"] = "CnT"; // "LocalUserData";
+$config['namespace'] = 'cnt';
+// ----------------------------------------
+
+$config['basename'] = 'cardsandtokens';
+$config['password'] = 'root';
+
+// --------------------------------------------------------------------------
+
+
+?>
